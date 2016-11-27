7b104813b91040563daa7aa93bc49a04a768d536
TharinduImesh <punchihewaimesh@gmail.com>
2016-11-27 09:22:12 -0800

    login-ui
    change main view back to MainForm

--- VisitorManagementSystem/VisitorManagementSystem/Program.cs
+++ VisitorManagementSystem/VisitorManagementSystem/Program.cs
@@ -16,8 +16,7 @@ namespace VisitorManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MainForm());
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
